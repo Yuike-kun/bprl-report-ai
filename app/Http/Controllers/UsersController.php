efb82bf36ef9b8d7e3c02890dc616b4a0315ec05
Yuike-kun <akbarhuntermob@gmail.com>
2026-09-26 16:07:36 +0800

    [Update] Change password validation to require a minimum length of 8 characters

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -117,7 +117,7 @@ class UsersController extends Controller
         $validated = $request->validate([
             'name'     => ['required', 'string', 'max:255'],
             'email'    => ['required', 'string', 'email', 'max:255', Rule::unique('users', 'email')->ignore($user->id)],
-            'password' => ['nullable', 'confirmed', Password::defaults()],
+            'password' => ['nullable', 'confirmed', 'min:8'],
             'role'     => ['required', 'string', Rule::in(User::ALL_ROLES)],
             'signature' => ['nullable', 'string'],
         ]);
